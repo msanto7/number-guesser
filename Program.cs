045ef0e4a22eebcfcdbdea8d079ca300a15927a0
MIKE <mike.santoro42@gmail.com>
2019-01-12 02:07:10 -0500

    proof of concept complete

--- Program.cs
+++ Program.cs
@@ -13,12 +13,87 @@ namespace numberGuesser
         static void Main(string[] args)
         {
 
-            string name = "Mike Santoro";
+            GetAppInfo();
 
-            //start code here //
-            Console.WriteLine(name);
-           
+            GreetUser();
+
+
+            while (true)
+            {
+
+                //int correctNum = 7;
+                Random random = new Random();
+                int correctNum = random.Next(1, 11);
+                int guessNum = 0;
+                Console.WriteLine("Guess a number between 1 and 10. ");
+
+                while (guessNum != correctNum)
+                {
+                    string input2 = Console.ReadLine();
+                    //validation
+                    if (!int.TryParse(input2, out guessNum))
+                    {
+                        PrintColorMessage(ConsoleColor.Red, "Please enter an actual number: ");
+                        continue;
+                    }
+                    guessNum = Int32.Parse(input2);
+
+                    if (guessNum != correctNum)
+                    {
+                        PrintColorMessage(ConsoleColor.Red, "Number is incorrect...guess again: ");
+                    }
+
+                }
+
+                PrintColorMessage(ConsoleColor.Yellow, "You guessed the correct number! ");
+
+                Console.WriteLine("Would you like to play again? [Y or N]");
+                string answer = Console.ReadLine().ToUpper();
+
+                if (answer == "Y")
+                {
+
+                } else if (answer == "N")
+                {
+                    return;
+                }
+
+
+            }
+
         } //Ends main
+
+        //**********************************************************
+
+        static void GetAppInfo()
+        {
+            // app variables
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Mike Santoro";
+
+            //text color
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+            //header
+            Console.WriteLine("{0}: Version: {1} by {2}", appName, appVersion, appAuthor);
+            Console.ResetColor();
+        }
+
+        static void GreetUser()
+        {
+            Console.WriteLine("What is your name? ");
+            // user input
+            string input = Console.ReadLine();
+            Console.WriteLine("Hello {0}, lets play a game...", input);
+        }
+
+        static void PrintColorMessage (ConsoleColor color, string message)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
 
 
     } //Ends class
---
+++ readME.md
@@ -0,0 +1,13 @@
+# C# Console Application - Number Guesser
+
+This is a very very basic C# application simply to reorient myself and my current machine with Visual Studio 2017 and the .NET framework. This code was created following the tutorial below. 
+
+## Program Explained
+
+This program simply prompts a user for a number between 1 and 10, and compares this guess to the randomly generator number. There is very basic input validation. 
+
+## Execution 
+
+If for some reason you would want to test this application...the app file is located in the /bin direcotry
+
+Traversy tutorial:	https://www.youtube.com/watch?v=GcFJjpMFJvI 
