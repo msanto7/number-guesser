ddc0ebfd854cc91be952fa18e8ac9a104c6514b1
MIKE <mike.santoro42@gmail.com>
2019-01-12 00:27:06 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+
+//Namespace
+namespace numberGuesser
+{
+
+    //Main Class
+    class Program
+    {
+
+        //Entry Point
+        static void Main(string[] args)
+        {
+
+            string name = "Mike Santoro";
+
+            //start code here //
+            Console.WriteLine(name);
+           
+        } //Ends main
+
+
+    } //Ends class
+
+
+} //Ends Namespace
